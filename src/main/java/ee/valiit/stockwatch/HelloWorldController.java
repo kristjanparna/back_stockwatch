3103d948b660a8e9705bcf17c48deece244b6118
kristjanparna <parna.kristjan@gmail.com>
2022-11-14 14:19:04 +0200

    created develop branch

--- src/main/java/ee/valiit/stockwatch/HelloWorldController.java
+++ src/main/java/ee/valiit/stockwatch/HelloWorldController.java
@@ -1,2 +1,14 @@
-package ee.valiit.stockwatch;public class HelloWorldController {
+package ee.valiit.stockwatch;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    @GetMapping("/hello/world")
+    public String helloWorld() {
+        return "Hello World!";
+    }
+
 }
