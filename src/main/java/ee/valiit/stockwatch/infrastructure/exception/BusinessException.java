4132d5c7cef6501a285c1cda254bdf08b39c8753
kristjanparna <parna.kristjan@gmail.com>
2022-11-22 09:33:28 +0200

    Added errors

---
+++ src/main/java/ee/valiit/stockwatch/infrastructure/exception/BusinessException.java
@@ -0,0 +1,15 @@
+package ee.valiit.stockwatch.infrastructure.exception;
+
+import lombok.Data;
+
+@Data
+public class BusinessException extends RuntimeException {
+    private final String message;
+    private final String errorCode;
+
+    public BusinessException(String message, String errorCode) {
+        super(message);
+        this.message = message;
+        this.errorCode = errorCode;
+    }
+}
