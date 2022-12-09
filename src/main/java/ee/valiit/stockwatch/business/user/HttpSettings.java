d0a001838258dcbd495e0e7f9fa1538504e3e782
Andres Orgla <aorgla@gmail.com>
2022-12-09 09:02:29 +0200

    Added delete from watchlist functionality

--- src/main/java/ee/valiit/stockwatch/business/user/HttpSettings.java
+++ src/main/java/ee/valiit/stockwatch/business/user/HttpSettings.java
@@ -9,7 +9,7 @@ public class HttpSettings {
 
     public static final String API_KEY = "X-RapidAPI-Key";
     public static final String API_HOST = "X-RapidAPI-Host";
-    public static final String API_KEY_VALUE = "b15672ba98msh7cd74569e47b173p1eb4ccjsnd188f0c74fa6";
+    public static final String API_KEY_VALUE = "143f650547msh3b6b23a8bcfaadfp102078jsn30a376c71534";
     public static final String API_HOST_VALUE = "apidojo-yahoo-finance-v1.p.rapidapi.com";
     public static final String BASE_URL = "https://apidojo-yahoo-finance-v1.p.rapidapi.com";
     public static final String BASE_URL_SEARCH = "https://apidojo-yahoo-finance-v1.p.rapidapi.com/stock/v2";
